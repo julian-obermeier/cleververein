e466a8a8619d37fb321ca76b590c74f023b7cc04
julian-obermeier <jeyohtv@gmail.com>
2026-09-16 21:42:43 +0200

    Apply form lifecycle guards

--- routes/forms.php
+++ routes/forms.php
@@ -9,21 +9,23 @@ Route::middleware(['installed', 'auth', 'verified', 'tenant'])->group(function (
     Route::get('/formulare', [FormController::class, 'index'])->name('forms.index');
     Route::post('/formulare', [FormController::class, 'store'])->name('forms.store');
     Route::get('/formulare/{form}/bearbeiten', [FormController::class, 'edit'])->name('forms.edit');
-    Route::put('/formulare/{form}', [FormController::class, 'update'])->name('forms.update');
-    Route::post('/formulare/{form}/veroeffentlichen', [FormController::class, 'publish'])->name('forms.publish');
+    Route::put('/formulare/{form}', [FormController::class, 'update'])->middleware('form-editable')->name('forms.update');
+    Route::post('/formulare/{form}/veroeffentlichen', [FormController::class, 'publish'])->middleware(['form-editable', 'form-publishable'])->name('forms.publish');
     Route::patch('/formulare/{form}/archivieren', [FormController::class, 'archive'])->name('forms.archive');
 
-    Route::post('/formulare/{form}/felder', [FormController::class, 'storeField'])->name('forms.fields.store');
-    Route::put('/formulare/{form}/felder/{field}', [FormController::class, 'updateField'])->name('forms.fields.update');
-    Route::delete('/formulare/{form}/felder/{field}', [FormController::class, 'destroyField'])->name('forms.fields.destroy');
-    Route::post('/formulare/{form}/felder/reihenfolge', [FormController::class, 'reorderFields'])->name('forms.fields.reorder');
+    Route::middleware('form-editable')->group(function (): void {
+        Route::post('/formulare/{form}/felder', [FormController::class, 'storeField'])->name('forms.fields.store');
+        Route::put('/formulare/{form}/felder/{field}', [FormController::class, 'updateField'])->name('forms.fields.update');
+        Route::delete('/formulare/{form}/felder/{field}', [FormController::class, 'destroyField'])->name('forms.fields.destroy');
+        Route::post('/formulare/{form}/felder/reihenfolge', [FormController::class, 'reorderFields'])->name('forms.fields.reorder');
 
-    Route::post('/formulare/{form}/workflows', [FormController::class, 'storeWorkflow'])->name('forms.workflows.store');
-    Route::patch('/formulare/{form}/workflows/{workflow}/aktivieren', [FormController::class, 'activateWorkflow'])->name('forms.workflows.activate');
-    Route::post('/formulare/{form}/workflows/{workflow}/schritte', [FormController::class, 'storeWorkflowStep'])->name('forms.workflow-steps.store');
-    Route::put('/formulare/{form}/workflows/{workflow}/schritte/{step}', [FormController::class, 'updateWorkflowStep'])->name('forms.workflow-steps.update');
-    Route::delete('/formulare/{form}/workflows/{workflow}/schritte/{step}', [FormController::class, 'destroyWorkflowStep'])->name('forms.workflow-steps.destroy');
-    Route::post('/formulare/{form}/workflows/{workflow}/reihenfolge', [FormController::class, 'reorderWorkflowSteps'])->name('forms.workflow-steps.reorder');
+        Route::post('/formulare/{form}/workflows', [FormController::class, 'storeWorkflow'])->name('forms.workflows.store');
+        Route::patch('/formulare/{form}/workflows/{workflow}/aktivieren', [FormController::class, 'activateWorkflow'])->name('forms.workflows.activate');
+        Route::post('/formulare/{form}/workflows/{workflow}/schritte', [FormController::class, 'storeWorkflowStep'])->name('forms.workflow-steps.store');
+        Route::put('/formulare/{form}/workflows/{workflow}/schritte/{step}', [FormController::class, 'updateWorkflowStep'])->name('forms.workflow-steps.update');
+        Route::delete('/formulare/{form}/workflows/{workflow}/schritte/{step}', [FormController::class, 'destroyWorkflowStep'])->name('forms.workflow-steps.destroy');
+        Route::post('/formulare/{form}/workflows/{workflow}/reihenfolge', [FormController::class, 'reorderWorkflowSteps'])->name('forms.workflow-steps.reorder');
+    });
 
     Route::get('/formulare/{form}/ausfuellen', [FormSubmissionController::class, 'fill'])->name('forms.fill');
     Route::post('/formulare/{form}/einreichen', [FormSubmissionController::class, 'store'])->name('forms.submit');
