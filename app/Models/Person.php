e8f8ad79702fb9a5f328464025a942694d907422
julian-obermeier <jeyohtv@gmail.com>
2026-09-16 15:38:17 +0200

    Link persons to member profiles

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Person extends Model
@@ -15,6 +16,11 @@ class Person extends Model
     protected $fillable = ['public_id', 'salutation', 'title', 'first_name', 'last_name', 'email', 'birth_date', 'contact_data'];
 
     protected $casts = ['birth_date' => 'date', 'contact_data' => 'array'];
+
+    public function members(): HasMany
+    {
+        return $this->hasMany(Member::class);
+    }
 
     public function getDisplayNameAttribute(): string
     {
