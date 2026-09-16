5f3d08abe1eb82b5a4bc47f02b1e953b4ce75ec0
julian-obermeier <jeyohtv@gmail.com>
2026-09-16 17:27:58 +0200

    Add contribution rate model

---
+++ app/Models/ContributionRate.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Concerns\BelongsToTenant;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ContributionRate extends Model
+{
+    use BelongsToTenant, SoftDeletes;
+
+    protected $fillable = ['name', 'code', 'amount', 'interval', 'billing_month', 'description', 'is_active'];
+
+    protected $casts = [
+        'amount' => 'decimal:2',
+        'billing_month' => 'integer',
+        'is_active' => 'boolean',
+    ];
+
+    public function rules(): HasMany
+    {
+        return $this->hasMany(ContributionRule::class);
+    }
+}
