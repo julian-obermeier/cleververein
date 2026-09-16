61258c43ad414fc91c69b41f060f2f746caad4c1
julian-obermeier <jeyohtv@gmail.com>
2026-09-16 19:49:11 +0200

    Add collective donation certificate routes

--- routes/finance_recovery_donations.php
+++ routes/finance_recovery_donations.php
@@ -9,6 +9,9 @@ Route::middleware(['installed', 'auth', 'verified', 'tenant'])->group(function (
 
     Route::post('/finanzen/spenden', [FinanceRecoveryDonationController::class, 'storeDonation'])->name('finance.donations.store');
     Route::post('/finanzen/spenden/{donation}/zuwendungsbestaetigung', [FinanceRecoveryDonationController::class, 'issueCertificate'])->name('finance.donations.certificates.issue');
+    Route::post('/finanzen/spenden/sammelbestaetigung', [FinanceRecoveryDonationController::class, 'issueCollectiveCertificate'])->name('finance.donations.collective-certificates.issue');
     Route::get('/finanzen/zuwendungsbestaetigungen/{certificate}/pdf', [FinanceRecoveryDonationController::class, 'certificatePdf'])->name('finance.donation-certificates.pdf');
     Route::patch('/finanzen/zuwendungsbestaetigungen/{certificate}/storno', [FinanceRecoveryDonationController::class, 'voidCertificate'])->name('finance.donation-certificates.void');
+    Route::get('/finanzen/sammelbestaetigungen/{collectiveCertificate}/pdf', [FinanceRecoveryDonationController::class, 'collectiveCertificatePdf'])->name('finance.donation-collective-certificates.pdf');
+    Route::patch('/finanzen/sammelbestaetigungen/{collectiveCertificate}/storno', [FinanceRecoveryDonationController::class, 'voidCollectiveCertificate'])->name('finance.donation-collective-certificates.void');
 });
