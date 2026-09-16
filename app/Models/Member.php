0ab0f0a578329ded2286d28cb27f79d5108f09f1
julian-obermeier <jeyohtv@gmail.com>
2026-09-16 15:59:41 +0200

    Connect member extended relations

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Models\Concerns\BelongsToTenant;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -35,5 +36,17 @@ class Member extends Model
     public function primaryMembership(): HasMany
     {
         return $this->hasMany(Membership::class)->where('is_primary', true);
+    }
+
+    public function households(): BelongsToMany
+    {
+        return $this->belongsToMany(Household::class, 'household_members')
+            ->withPivot(['relationship', 'is_primary_contact'])
+            ->withTimestamps();
+    }
+
+    public function functionAssignments(): HasMany
+    {
+        return $this->hasMany(FunctionAssignment::class);
     }
 }
