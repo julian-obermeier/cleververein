358a2798731193a9cf2d536342e44aa153646d64
julian-obermeier <jeyohtv@gmail.com>
2026-09-16 14:38:03 +0200

    fix: bind user person foreign key to persons table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('person_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('person_id')->nullable()->constrained('persons')->nullOnDelete();
             $table->foreignId('current_tenant_id')->nullable()->constrained('tenants')->nullOnDelete();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
